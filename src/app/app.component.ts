6587cdeda32098fd9c164c8d1678f4dd90bfa523
billcuthbert <96billc@gmail.com>
2022-05-22 22:22:18 +0100

    vercel test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Colyseus from "colyseus.js"; // not necessary if included via <script> tag.
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skip-fe';
+
+
+  client = new Colyseus.Client('ws://localhost:2567')
+
+  ngOnInit() {
+    // this.client.getAvailableRooms().then(console.log)
+    this.client.joinOrCreate("my_room").then(function (room) {
+      console.log(room.sessionId, "joined", room.name);
+
+    }).catch(console.error)
+  }
 }
