39bc3ccdd2774ed5ce44bbb8dbb0cce19263767a
billcuthbert <96billc@gmail.com>
2022-05-24 23:25:15 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import * as Colyseus from "colyseus.js"; // not necessary if included via <script> tag.
 
 @Component({
@@ -9,14 +10,25 @@ import * as Colyseus from "colyseus.js"; // not necessary if included via <scrip
 export class AppComponent implements OnInit {
   title = 'skip-fe';
 
-
+  messages: any[] = [];
+  room!: Colyseus.Room<unknown>
   client = new Colyseus.Client('wss://honey-breezy-piranha.glitch.me/:2567')
 
   ngOnInit() {
-    // this.client.getAvailableRooms().then(console.log)
-    this.client.joinOrCreate("my_room").then(function (room) {
+    this.client.joinOrCreate("my_room").then(room => {
+      this.room = room
       console.log(room.sessionId, "joined", room.name);
 
+      room.onMessage("*", (message) => {
+        if (String(message).startsWith(this.room.sessionId))
+          message += " (you)"
+        this.messages.push(message);
+      });
+
     }).catch(console.error)
+  }
+
+  click(fruit: string) {
+    if (this.room) this.room.send(fruit)
   }
 }
